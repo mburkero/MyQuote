27d4f96749b83c74d507e4e4701ec0bb42ef67ec
mburkero <max.burkerowe@gmail.com>
2024-09-13 04:45:36 +0000

    13/09/2024 Category model added to database

---
+++ db/migrate/20240913043019_create_categories.rb
@@ -0,0 +1,10 @@
+class CreateCategories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :categories do |t|
+      t.string :categoryName null:false
+
+      t.timestamps
+    end
+    add_index :categories, :categoryName, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_13_032749) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_13_043019) do
+  create_table "categories", force: :cascade do |t|
+    t.string "categoryName"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["categoryName"], name: "index_categories_on_categoryName", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "fname", null: false
     t.string "lname", null: false
