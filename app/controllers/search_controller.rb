7a7f61510ee92ee33a11d3033695f9d206bb81f4
mburkero <max.burkerowe@gmail.com>
2024-10-14 16:09:37 +0000

    15/10/2024 Completed Quote Search Function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
   def index
-    # query returns all quotes that are linked to a searched category. Frontend implementation is found in app/views/search/index.html.erb
+    # query returns all quotes that are linked to a searched category so long as they are marked as public. Frontend implementation is found in app/views/search/index.html.erb
     category_query = params[:category_query]
       if category_query.present?
-        @quote_match = Quote.joins(:author, :quote_categories, categories).where("categories.categoryName LIKE ?", "%#{category_query}%").distinct
+        @quote_match = Quote.joins(:author, :quote_categories, :categories).where("quotes.isQuotePublic = true AND categories.categoryName LIKE ?", "%#{category_query}%").distinct
       end
   end
 end
